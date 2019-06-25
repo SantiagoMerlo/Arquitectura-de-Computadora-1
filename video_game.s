c8f450d89b691998884268135f6c800311c56071
SantiagoMerlo <37404882+SantiagoMerlo@users.noreply.github.com>
2019-06-25 09:25:41 -0300

    Update video_game.s

--- video_game.s
+++ video_game.s
@@ -1,3 +1,91 @@
+
+.text
+.extern press_key
+.extern output
+.extern delay
+.extern tiempo
+.global video_game
+
+video_game:
+	PUSH {R4-R7, LR}
+_game_init:
+	MOV R4,#0 
+_game_move:
+	BL press_key
+	CMP R0, #1
+	BNE _game_out
+	MOV R0,#1
+	BL output	
+	LDR R0,=tiempo
+	LDR R0, [R0]
+	BL delay
+loop:				//mostrar "autofantastico"
+	ADD R4,R4,#1
+        MOV R1,#1		//valor de funcion imput/output
+	MOV R5,R4		// 2 elevado a r4
+        MOV R2,#0            	// tipo de salida
+        B potencia
+cond_a:				//condicion de tipo 0
+	MOV R0,R1
+        BL output
+        BL press_key
+	CMP R0,#1
+	BNE _game_out
+	LDR R0,=tiempo
+	LDR R0, [R0]
+	BL delay
+	CMP R4,#7 	 
+        BNE loop		//--> Return loop
+	
+	MOV R7,#1
+general:	
+	MOV R3,R7		//contador j
+loop2:
+        MOV R1,#1
+        MOV R2,#1
+        MOV R5,R7
+        B potencia
+cond_b:				//condicion de tipo 1
+        MOV R6,R1
+        SUB R6,R6,#1		//arreglo - 1 = todas las luces prendidas desde un punto
+        MOV R5,R3
+        MOV R2,#2
+        MOV R1,#1
+	B potencia
+cond_c:				//condicion de tipo obligatoria (tipo 2)
+	ADD R0,R6,R1
+	BL output
+	LDR R0,=tiempo
+	LDR R0, [R0]
+	BL delay
+	BL press_key
+	CMP R0,#1
+	BNE _game_out
+	ADD R3,R3,#1		//j++
+	CMP R3,#8
+        BNE loop2
+	ADD R7,R7,#1		//contador k++
+	CMP R7,#8
+        BNE general
+        B _game_init
+
+potencia:			//funcion calcular potencia de 2
+    	ADD R1,R1,R1
+    	SUBS R5,R5,#1
+        BNE potencia
+        CMP R2,#0
+        BEQ cond_a		//--> salida a primer loop
+        CMP R2,#1
+        BEQ cond_b		//salida para valor de arreglo menos uno
+        B cond_c		//salida para valor arreglo
+_game_out:
+	POP {R4-R7, PC}
+.data
+
+.end
+
+
+/* intento 1 con arreglo
 .text
 
 .extern press_key
@@ -19,6 +107,7 @@ _game_move:
 	STRB R0,[R6]	//cargamos en R6(+R4) el dato de R0 --> STRB R0,[R6,#0]
 	BL output	
 	LDR R0,=tiempo	//Velocidad del delay, modificado en press_key
+	LDR R0, [R0]
 	BL delay
 loop:			//En este loop se definen los valores de el arreglo
 	MOV R1,#1	//instanciamos el valor a elevar
@@ -35,6 +124,7 @@ BNE potencia		//condicion de salida, R5 = 0
 	MOV R0,R1
 	BL output
 	LDR R0,=tiempo
+	LDR R0, [R0]
 	BL delay
 	CMP R4,#7 	//contador, empieza en el loop con 1
 BNE loop
@@ -48,6 +138,7 @@ loop2:
 	ADD R0,R0,R1	// si teniamos un R0 = 00000010 + R1= 11100000 = 11100010
 	BL output
 	LDR R0,=tiempo
+	LDR R0, [R0]
 	BL delay
 	BL press_key
 	CMP R0, #1
@@ -74,92 +165,6 @@ arreglo:	// 	filtro		valor anterior menos uno
       .byte 6	//64--> 00000010 -->	11111100
       .byte 7	//128-> 00000001 -->	11111110
 .end
-
-//*
-
-.text
-
-.extern press_key
-.extern output
-.extern delay
-.extern tiempo
-.global video_game
-
-video_game:
-	PUSH {R4-R7, LR}
-_game_init:
-	MOV R4,#0 
-_game_move:
-	BL press_key
-	CMP R0, #1
-	BNE _game_out
-	MOV R0,#1
-	BL output	
-	LDR R0,=tiempo
-	BL delay
-loop:
-	ADD R4,R4,#1
-        MOV R1,#1
-	MOV R5,R4	
-        MOV R2,#0            
-        B potencia
-cond_a:	
-	MOV R0,R1
-        BL output
-        BL press_key
-	CMP R0,#1
-	BNE _game_out
-	LDR R0,=tiempo
-	BL delay
-	CMP R4,#7 	 
-        BNE loop
-	MOV R7,#1
-general:	
-	MOV R3,R7
-loop2:
-        MOV R1,#1
-        MOV R2,#1
-        MOV R5,R7
-        B potencia
-cond_b:
-        MOV R6,R1
-        SUB R6,R6,#1
-        MOV R5,R3
-        MOV R2,#2
-        MOV R1,#1
-	B potencia
-cond_c:
-	ADD R0,R6,R1
-	BL output
-	LDR R0,=tiempo
-	BL delay
-	BL press_key
-	CMP R0,#1
-	BNE _game_out
-	ADD R3,R3,#1
-	CMP R3,#8
-        BNE loop2
-	ADD R7,R7,#1	//contador k++
-	CMP R7,#8
-        BNE general
-        B _game_init
-
-potencia:                //r5 la cantidad de veces que se eleva
-    	ADD R1,R1,R1
-    	SUBS R5,R5,#1
-        BNE potencia
-        CMP R2,#0
-        BEQ cond_a
-        CMP R2,#1
-        BEQ cond_b
-        B cond_c
-_game_out:
-	POP {R4-R7, PC}
-.data
-
-.end
-
-
-*//
+*/
 
    
